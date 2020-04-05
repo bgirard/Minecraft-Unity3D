0b181861a6e61b11f16558395dd783b4439057e9
Benoit Girard <b56girard@gmail.com>
2020-04-05 01:10:55 -0700

    Better lighting at night

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -2,31 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DayNightCycle : MonoBehaviour {
+public class DayNightCycle : MonoBehaviour
+{
     public float speed = 20;
     private float rotation = 40;
     private float skyboxRotation = 0;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
 
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         rotation = rotation + Time.deltaTime * speed;
-        if (rotation % 356 >= 260) {
+        if (rotation % 356 >= 260)
+        {
             rotation = 1;
         }
         transform.eulerAngles = new Vector3(rotation, -30, 0);
         Light light = GetComponent<Light>();
-        if (rotation >= 150) {
+        if (rotation >= 150)
+        {
             light.intensity = Mathf.Lerp(1, 0, (rotation - 150) / (180 - 150));
-        } else if (rotation <= 30) {
+        }
+        else if (rotation <= 30)
+        {
             light.intensity = Mathf.Lerp(0, 1, (rotation - 0) / (30 - 0));
-        } else {
+        }
+        else
+        {
             light.intensity = 1;
         }
+        light.intensity = 0;
 
         RenderSettings.skybox.SetFloat("_Exposure", light.intensity);
         skyboxRotation += Time.deltaTime * speed / 10f;
