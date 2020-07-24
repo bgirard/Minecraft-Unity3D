02714c50776ab4eabdef3cd3bfc75cd8e091f5fc
Sirius777RU <epicdust-game@mail.ru>
2020-07-24 06:00:04 +0300

    Settings, cleanup, new font, apple, chicken, slime textures, meshes and animation just to see if fits.
    [New] Settings system that use ini files. Currently provide options to enable\disable postprocessing, underwater effects and to select antialiasing. (Used cemdervis/SharpConfig from it's githhub repo for ini functionality.)
    [New] Added new pixelated font.
    [New] Added animated slime, chicken and apple geometry, textures and materials. Enable "StyleTest" in scene to see them.
    
    [Change] Renamed "Product Name" to "Community Voxel Project".
    [Change] Applied icon for application.
    
    [Fix] Fixed that small white ripple on water in build.
    
    [Other] More folders cleanup.

--- Assets/Scripts/Terrain/TerrainModifier.cs
+++ Assets/Scripts/Terrain/TerrainModifier.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class TerrainModifier : MonoBehaviour
@@ -15,6 +13,7 @@ public class TerrainModifier : MonoBehaviour
     {
 
     }
+    
     private void Update()
     {
         if (PauseMenu.pause)    return;
---
+++ Assets/Scripts/UI/UIVideoSettings.cs
@@ -0,0 +1,77 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.UI;
+
+//Able to read, write and apply video-settings.  
+[RequireComponent(typeof(UISettings))]
+public class UIVideoSettings : MonoBehaviour
+{
+    public PostProcessLayer postProcessLayer;
+    public PostProcessProfile defaultPostProcessProfile;
+    public PostProcessVolume underwaterEffectsVolume;
+
+    [Space(10)] 
+    public TMP_Dropdown antialiasing;
+    
+    [Space(10)]
+    public Toggle   underwaterEffects;
+    public Toggle   bloom,
+                    ambientOcclusion,
+                    vignette,
+                    motionBlur,
+                    depthOfField,
+                    colorGrading,
+                    autoExposure;
+
+    private UISettings uiSettings;
+        
+    private void Start()
+    {
+        uiSettings = GetComponent<UISettings>();
+
+        Load();
+        Local();
+    }
+
+    //Call if settings ready for apply.
+    public void ChangeSettings()
+    {
+        Local();
+        uiSettings.Save();
+    }
+
+    //Load settings from configuration. Use UISettings Load() to load configuration from file.
+    public void Load()
+    {
+        antialiasing.value     = UISettings.settingsData["Video Settings"]["Antialiasing"].IntValue;
+        underwaterEffects.isOn = UISettings.settingsData["Video Settings"]["Underwater Effects"].BoolValue;
+        
+        bloom.isOn            = UISettings.settingsData["Video Settings"]["Bloom"].BoolValue;
+        ambientOcclusion.isOn = UISettings.settingsData["Video Settings"]["Ambient Occlusion"].BoolValue;
+        vignette.isOn         = UISettings.settingsData["Video Settings"]["Vignette"].BoolValue;
+        motionBlur.isOn       = UISettings.settingsData["Video Settings"]["Motion Blur"].BoolValue;
+        depthOfField.isOn     = UISettings.settingsData["Video Settings"]["Depth Of Field"].BoolValue;
+        autoExposure.isOn     = UISettings.settingsData["Video Settings"]["Auto Exposure"].BoolValue;
+        colorGrading.isOn     = UISettings.settingsData["Video Settings"]["Color Grading"].BoolValue;
+
+        Local();
+    }
+
+    //Apply settings to game.
+    private void Local()
+    {
+        postProcessLayer.antialiasingMode = (PostProcessLayer.Antialiasing) antialiasing.value;
+        underwaterEffectsVolume.enabled = underwaterEffects.isOn;
+        
+        defaultPostProcessProfile.GetSetting<Bloom>().enabled.value            = bloom.isOn;
+        defaultPostProcessProfile.GetSetting<AmbientOcclusion>().enabled.value = ambientOcclusion.isOn;
+        defaultPostProcessProfile.GetSetting<Vignette>().enabled.value         = vignette.isOn;
+        defaultPostProcessProfile.GetSetting<MotionBlur>().enabled.value       = motionBlur.isOn;
+        defaultPostProcessProfile.GetSetting<DepthOfField>().enabled.value     = depthOfField.isOn;
+        defaultPostProcessProfile.GetSetting<AutoExposure>().enabled.value     = autoExposure.isOn;
+        defaultPostProcessProfile.GetSetting<ColorGrading>().enabled.value     = colorGrading.isOn;
+    }
+    
+    
+}
